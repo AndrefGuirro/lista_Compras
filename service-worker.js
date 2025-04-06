f5ab5400f78cc165c8f8f179b47eaf57b8a770bb
AndrefGuirro <131619465+AndrefGuirro@users.noreply.github.com>
2025-04-06 15:13:50 -0300

    .
    .

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
-const cacheName = 'lista-compras-v1';
+const cacheName = 'lista-compras-v2';
+
 const arquivosParaCache = [
-    './',
-    './index.html',
-    './app.js',
-    './manifest.json',
-    './imagens/logoCompra192.png',
-    './imagens/logoCompra512.png',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
-  ];
-  
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './imagens/logoCompra192.png',
+  './imagens/logoCompra512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName)
+    caches.open(cacheName) // Aqui usamos a variável corretamente
       .then(cache => cache.addAll(arquivosParaCache))
   );
 });
